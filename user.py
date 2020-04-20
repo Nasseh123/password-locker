e055cbbad8518f6bb1617e9d760aa4becc8e8b74
Manasseh Bundi <Manassehbundi2017@gmail.com>
2020-04-20 19:15:18 +0300

    Made new addition to testing the user section where its involved in validation of creating a new account and login in with the right credentials.Unnitest to see if the program can delete,add multiple users,find a user from the list and check if a user has been savid and added to the user_list array was succesful.Also the relevant methods concerning the same functionalities were made in the user.py file

--- user.py
+++ user.py
@@ -1,4 +1,4 @@
-class user:
+class User:
     """
     Class for holding the login and password credentials of a certain user.
     also Its required to enable validations of getting the right users
@@ -8,3 +8,48 @@ class user:
         self.password=password
  
     user_list=[]
+    # method for saving a user by appending it to the user_list
+
+    def save_user(self):
+
+        '''
+        save_user method for saving a user by appending it to the user_list
+        '''
+        User.user_list.append(self)
+
+    # method for deleting users
+    def delete_contact(self):
+
+        '''
+        delete_contact method deletes a saved contact from the contact_list
+        '''
+
+        User.user_list.remove(self)
+
+    @classmethod
+    def find_by_username(cls,username):
+        '''
+        Method that takes in a number and returns a contact that matches that number.
+
+        Args:
+            number: Phone number to search for
+        Returns :
+            Contact of person that matches the number.
+        '''
+        for User in cls.user_list:
+            if User.username== username:
+                return User
+
+    @classmethod
+    def Username_exist(cls,username):
+        '''
+        Method that checks if a contact exists from the contact list.
+        Args:
+            number: Phone number to search if it exists
+        Returns :
+            Boolean: True or false depending if the contact exists
+        '''
+        for User in cls.user_list:
+            if User.username==username:
+                return True
+        return False
